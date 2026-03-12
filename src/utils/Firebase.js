4272e593df710267e26b47477e0328b5b1ba51a0
riyacoding007 <riyacoding007@gmail.com>
2026-03-12 14:07:58 +0530

    fix firebase config

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -3,14 +3,24 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDBFrBYnpQsUat9hiI2zApgpuF9itMoMkI",
+  authDomain: "traffic-complaint-system.firebaseapp.com",
+  projectId: "traffic-complaint-system",
+  storageBucket: "traffic-complaint-system.firebasestorage.app",
+  messagingSenderId: "586648171354",
+  appId: "1:586648171354:web:584b3c4f8bf02a1649f963",
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
